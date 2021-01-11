0c35aac907d9389278e3da5d26d8620b391116a9
thecodeplanner <thecodeplanner@gmail.com>
2021-01-11 15:29:20 -0500

    commit tree 4ce95156fe0f0b53083f9f62f44d61d652820927

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props)
+
+  if (!props.isPublished) return null
+
+  return (
+    <div>
+      <h1>{props.articleText}</h1>
+      <p>{props.minutesToRead}</p>
+    </div>
+  )
 }
 
 export default BlogContent;
